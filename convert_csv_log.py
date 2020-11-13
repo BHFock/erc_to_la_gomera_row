af6ae13f71bd8a08dbaef6a8a28d20fe26d84abc
chrisgs <chrisgs16@hotmail.com>
2020-11-13 21:26:41 +0000

    Update log column names

--- convert_csv_log.py
+++ convert_csv_log.py
@@ -13,4 +13,9 @@ log_path = 'log/land_crew.log'
 all_data = pd.read_csv(csv_path)
 log_data = all_data.iloc[:, 0:3]
 log_data['Date'] = log_data['Date'].apply(lambda x: convert_date(x))
+
+log_data.rename(columns={'Date':'date'}, inplace=True)
+log_data.rename(columns={'Meters':'meter'}, inplace=True)
+log_data.rename(columns={'Name':'name'}, inplace=True)
+
 log_data.to_csv(log_path, index=False, sep=';')
